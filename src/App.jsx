f77d041542c8452e720d432dfd4c820445e3351e
namae101 <sovannarongkoek@gmail.com>
2021-12-07 16:21:59 +0700

    import from replit

---
+++ src/App.jsx
@@ -0,0 +1,208 @@
+import React, { useEffect, useState } from "react"; import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+import { ethers } from "ethers";
+import myEpicNft from './utils/MyEpicNFT.json';
+// Constants
+const TWITTER_HANDLE = 'namae_101';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0xa4B7Ee28520f434655E2156D7FAf69Dd4e046251";
+const App = () => {
+
+
+  /*
+ * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
+ */
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [numberOfMintedNFT, setNumberOfMintedNFT] = useState(0);
+  /*
+  * Gotta make sure this is async.
+  */
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+      await checkChain();
+    }
+
+    /*
+    * Check if we're authorized to access the user's wallet
+    */
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    /*
+    * User can have multiple authorized accounts, we grab the first one if its there!
+    */
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found")
+    }
+  }
+
+  const checkChain = async () => {
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    console.log("Connected to chain " + chainId);
+
+    // String, hex code of the chainId of the Rinkebey test network
+    const rinkebyChainId = "0x4";
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
+  }
+  /*  
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getTotalOfMintedNFT = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+        const total = await connectedContract.getTotalNFTsMintedSoFar();
+        setNumberOfMintedNFT(total);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  // Setup our listener.
+
+  useEffect(() => {
+    let connectedContract;
+
+    const onNFTMinted = (from, tokenId) => {
+      console.log(from, tokenId.toNumber())
+      alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+      getTotalOfMintedNFT();
+    }
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+        connectedContract.on("NewEpicNFTMinted", onNFTMinted);
+        console.log("Setup event listener!")
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+    return () => {
+      if (connectedContract) {
+        connectedContract.off("NewEpicNFTMinted", onNFTMinted)
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    getTotalOfMintedNFT();
+  }, [])
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+              <>
+                <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+                  Mint NFT
+            </button>
+                <div className="numberOfMinted">{`${numberOfMintedNFT} / ${TOTAL_MINT_COUNT}`} </div>
+              </>
+            )}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
